28f300cd2936ca380644f11f8cebe874b91bfa53
viswachand <viswachand19@gmail.com>
2025-07-21 13:36:09 -0400

    updated the App.js

--- src/App.js
+++ src/App.js
@@ -3,21 +3,23 @@ import "./App.css";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
 import HomeScreen from "./homeScreen";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 
 function App() {
   const [config, setConfig] = useState(null);
-  const isFetched = useRef(false); 
+  const isFetched = useRef(false);
 
   useEffect(() => {
- 
-
     if (!isFetched.current) {
       const fetchConfig = async () => {
         try {
-          const response = await fetch("https://raw.githubusercontent.com/viswachand/myResumeData/refs/heads/main/Data.json");
+          const response = await fetch(
+            "https://raw.githubusercontent.com/viswachand/myResumeData/refs/heads/main/Data.json"
+          );
           const jsonData = await response.json();
           setConfig(jsonData);
-          isFetched.current = true; 
+          isFetched.current = true;
         } catch (error) {
           console.error("Error fetching config:", error);
         }
@@ -30,7 +32,27 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        {config ? <HomeScreen config={config} /> : <p>Loading...</p>}
+        {config ? (
+          <HomeScreen config={config} />
+        ) : (
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100vh",
+              flexDirection: "column",
+              gap: 2,
+            }}
+          >
+            <CircularProgress color="primary" />
+            <p
+              style={{ fontWeight: "bold", fontSize: "1.2rem", color: "#555" }}
+            >
+              Loading your personalized resume...
+            </p>
+          </Box>
+        )}
       </div>
     </ThemeProvider>
   );
